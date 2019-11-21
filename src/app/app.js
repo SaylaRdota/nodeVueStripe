c3766c52ce32e388ae004f8db9549ae11861687a
Michel Yanes López <michelyaneslopez@gmail.com>
2019-11-21 08:21:40 -0800

    Delete app.js

--- src/app/app.js
+++
@@ -1,8 +0,0 @@
-const app = require('./config/server')
-const Sequelize = require('sequelize');
-require ('./app/api/rest/payStripe')(app)
-
-//iniciar el sevidor
-app.listen(app.get('port'), ()=>{
-    console.log('Server on port ', app.get('port'))
-})
