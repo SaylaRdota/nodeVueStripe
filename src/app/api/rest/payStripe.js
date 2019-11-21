72fa0182b634ff09f13d75b5b4969d82cc65933a
Michel Yanes López <michelyaneslopez@gmail.com>
2019-11-21 11:10:09 -0800

    Update payStripe.js

--- src/app/api/rest/payStripe.js
+++ src/app/api/rest/payStripe.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
                 .then( customer => {
                     console.log('Customer: ', customer)
                     stripe.charges.create({
-                            amount: req.body.cant.toString(),
+                            amount: '20',
                             currency: "usd",
                             customer: customer.id,
                             description: 'Esta es la descripcion del producto'
